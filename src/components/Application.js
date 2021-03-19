90a5a810bcc7233905ac98a3ae4bdb6f6f9614fc
Eiman <imansawi>
2021-03-18 21:17:22 -0400

    Use axios to retreive days data using the end point api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import axios from "axios";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const appointments = [
   {
@@ -73,7 +58,13 @@ const interviewers = [
 ];
 
 export default function Application(props) {
-  //const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`)
+      .then(response => setDays(response.data));
+  }, []);
 
   const schedule = appointments.map((anAppointment) => (
     <Appointment
@@ -96,16 +87,14 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={"Monday"}
-            setDay={(day) => console.log(day)}
-          />
+        
+        <DayList days={days} day={day} setDay={setDay} />
+          
         </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
         />
       </section>
       <section className="schedule">
